3645c980908dd1725903a3636adfcb87b86918f8
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-12-01 11:52:14 +0330

    adding constants folder

---
+++ constants/index.tsx
@@ -0,0 +1,12 @@
+const images = {
+  authLogin: "/images/auth-login.png",
+  authRegister: "/images/auth-register.png",
+  person: "/images/person.jpg",
+  notFound: "/images/404.svg",
+  notAllowed: "/images/not-allowed.png",
+  error: "/images/sad.png",
+  noImage: "/images/no-image.jpg",
+  emptyFolder: "/images/folder-empty.svg",
+};
+
+export { images };
