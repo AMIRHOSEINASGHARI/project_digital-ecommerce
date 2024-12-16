64d494e1f3ac0e8ba05de452cddaf881413f1d62
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-12-16 21:25:44 +0330

    useHandleSearchParams hook

---
+++ hooks/index.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+// react
+import { Dispatch, SetStateAction, useEffect } from "react";
+// next
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+
+export const useHandleSearchParams = (
+  name?: string,
+  setValue?: Dispatch<SetStateAction<string>>
+) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { push, replace } = useRouter();
+
+  const handleSetQuery = (value: string) => {
+    if (!!name?.trim()) {
+      const params = new URLSearchParams(searchParams);
+
+      if (value) {
+        params.set(name, value);
+      } else {
+        params.delete(name);
+      }
+
+      const newRoute = `${pathname}?${params.toString()}`;
+      push(newRoute);
+    } else {
+      return;
+    }
+  };
+
+  const handleDeleteQuery = (value: string) => {
+    const params = new URLSearchParams(searchParams);
+
+    if (params?.has(value)) {
+      params.delete(value);
+    }
+
+    let newRoute = `${pathname}?${params.toString()}`;
+
+    if (params.size === 0) {
+      newRoute = pathname;
+    }
+
+    replace(newRoute);
+  };
+
+  useEffect(() => {
+    if (!name?.trim() || !setValue) return;
+
+    const params = new URLSearchParams(searchParams);
+    setValue(params.get(name) || "");
+  }, [name, setValue, searchParams]);
+
+  return {
+    handleSetQuery,
+    handleDeleteQuery,
+    replace,
+    push,
+    pathname,
+    searchParams,
+  };
+};
